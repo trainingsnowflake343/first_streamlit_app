fc0941e9467ef6d1eac75eecd7fcd7139dcf38da
trainingsnowflake343 <121413062+trainingsnowflake343@users.noreply.github.com>
2022-12-28 16:04:17 +1100

    Update streamlit_app.py
    def get_fruityvice_data

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,32 +33,38 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
 
+# create the repeatable code block (called a function)
+def get_fruityvice_data(this_fruit_choice):
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   return fruityvice_normalized
+
 #New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 
 try:
 
-   fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')
    if not fruit_choice:
       streamlit.error("Please select a fruit to get information.")
    else:
-    
-    streamlit.write('The user entered ', fruit_choice)
+      back_from_function = get_fruityvice_data(fruit_choice)
+      streamlit.dataframe(back_from_function)
 
               # basic1 import requests 
               # basic1 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
               # basic1 streamlit.text(fruityvice_response.json()) #just writes the raw json data to the screen
 
     #import requests 
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
               # basic2 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
               # basic1 streamlit.text(fruityvice_response.json()) #just writes the raw json data to the screen
 
     # normalised json output to rows & columns
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
     # display on screen
-    streamlit.dataframe(fruityvice_normalized)
+    #streamlit.dataframe(fruityvice_normalized)
     
 except URLError as e:
   streamlit.error()
